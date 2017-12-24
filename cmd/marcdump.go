8954dd6391b8f950a337020fb185149d67ee69e3
Gregory Siems <gsiems@gmail.com>
2017-12-23 19:04:57 -0600

    MARC dump utility to dump a MARC record as "pretty printed" text.

---
+++ cmd/marcdump.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+
+	"github.com/gsiems/go-marc21/pkg/marc21"
+)
+
+func main() {
+
+	var marcfile string
+	if len(os.Args) > 1 {
+		marcfile = os.Args[1]
+	}
+
+	if marcfile == "" {
+		showHelp()
+	}
+
+	fi, err := os.Open(marcfile)
+	if err != nil {
+		log.Fatal(fmt.Printf("File open failed: %q", err))
+	}
+	defer fi.Close()
+
+	for {
+		rec, err := marc21.ParseNextRecord(fi)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(rec)
+	}
+}
+
+func showHelp() {
+	fmt.Println(os.Args[0])
+	fmt.Println("   Dumps a MARC file as \"pretty printed\" text.")
+	fmt.Printf("    Usage: %s <MARC file to dump>\n", os.Args[0])
+	fmt.Println()
+	os.Exit(0)
+}
