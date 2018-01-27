8a25ac83bdb99e79ee275581726465e03b82f855
Gregory Siems <gsiems@gmail.com>
2018-01-27 13:08:11 -0600

    Add smoke test for marcsplit

--- cmd/test-build.sh
+++ cmd/test-build.sh
@@ -9,9 +9,12 @@ go build xml2marc.go
 echo "Building marc2xml"
 go build marc2xml.go
 
+echo "Building marcsplit"
+go build marcsplit.go
+
 echo ""
 echo "Testing marcdump"
-time ./marcdump ../git_ignore/malc-20171213.mrc > marcdump.out
+time ./marcdump ../git_ignore/malc-20180115.mrc > marcdump.out
 
 echo ""
 echo "Testing xml2marc"
@@ -19,7 +22,11 @@ time ./xml2marc ../git_ignore/collection.xml > xml2marc.out
 
 echo ""
 echo "Testing marc2xml"
-time ./marc2xml ../git_ignore/malc-20171213.mrc > marc2xml.out
+time ./marc2xml ../git_ignore/malc-20180112.mrc > marc2xml.out
 
-# marksplit
+echo ""
+echo "Testing marcsplit"
+[ -d split_out ] && rm split_out/*.mrc
+[ -d split_out ] || mkdir split_out
+time ./marcsplit -m ../git_ignore/malc-20180112.mrc -d split_out
 
