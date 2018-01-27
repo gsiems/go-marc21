b20e9122c6f00cc435f3fdf8dda9e1d986743ddc
Gregory Siems <gsiems@gmail.com>
2018-01-27 12:12:15 -0600

    Update copyright notice

--- pkg/marc21/marcxml.go
+++ pkg/marc21/marcxml.go
@@ -1,4 +1,4 @@
-// Copyright 2017 Gregory Siems. All rights reserved.
+// Copyright 2017-2018 Gregory Siems. All rights reserved.
 // Use of this source code is governed by the MIT license
 // that can be found in the LICENSE file.
 
